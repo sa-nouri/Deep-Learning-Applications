1fd78d0da5f1720a672404c25d3e5631a369b03a
Salar Nouri <42338270+SalarNouri@users.noreply.github.com>
2020-04-06 16:33:38 +0430

    Update Gaussian_RBF_Network.py

--- Multilayer Perceptron/Gaussian_RBF_Network.py
+++ Multilayer Perceptron/Gaussian_RBF_Network.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import pickle
 
 def load_dataset():
-    train_data = np.loadtxt("/Users/ali/Downloads/mnist_train.csv", delimiter=',')
-    test_data = np.loadtxt("/Users/ali/Downloads/mnist_test.csv", delimiter=',')
+    train_data = np.loadtxt("/Users/Salar/Downloads/mnist_train.csv", delimiter=',')
+    test_data = np.loadtxt("/Users/Salar/Downloads/mnist_test.csv", delimiter=',')
     tr_data = np.asarray(train_data[:,1:])
     tr_label = np.asarray(train_data[:,:1]).astype(int)
 
